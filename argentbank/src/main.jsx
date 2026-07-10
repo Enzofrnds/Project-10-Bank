0142fc39b59be2e83c5a34969ab09a3e37f51244
Enzofrnds <enzofernandes3@outlook.fr>
2026-07-10 11:10:10 +0200

    feat: ajout du footer

--- argentbank/src/main.jsx
+++ argentbank/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './index.css'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   </StrictMode>,
 )
