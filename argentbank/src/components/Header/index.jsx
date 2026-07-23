310d93c9df3961b038ba74ce9a62cd1d8552e62b
Enzofrnds <enzofernandes3@outlook.fr>
2026-07-23 05:19:19 +0200

    feat: ajout de la fonction de deconnexion

--- argentbank/src/components/Header/index.jsx
+++ argentbank/src/components/Header/index.jsx
@@ -1,18 +1,42 @@
 import { NavLink } from 'react-router'
 import Logo from '../../assets/argentBankLogo.png'
 import './Header.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../../features/userSlice'
 
 function Header() {
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+    const firstName = useSelector((state) => state.user.user?.firstName)
+    const lastName = useSelector((state) => state.user.user?.lastName)
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
+
     return (
         <header className="header">
             <nav className="header__nav">
                 <NavLink to="/" className="header__nav__logo">
                     <img src={Logo} alt="Argent Bank Logo" />
                 </NavLink>
-                <NavLink to="/SignIn" className="header__nav__link">
-                    <i className="fa fa-user-circle"></i>
-                    <p>Sign In</p>
-                </NavLink>
+                {isLoggedIn ? (
+                    <NavLink to="/User" className="header__nav__link">
+                        <i className="fa fa-user-circle"></i>
+                        <p>{firstName} {lastName}</p>
+                    </NavLink>
+                ) : null}
+                {isLoggedIn ? (
+                    <NavLink to="/SignIn" className="header__nav__link" onClick={handleLogout}>
+                        <i className="fa fa-sign-out"></i>
+                        <p>Sign Out</p>
+                    </NavLink>
+                ) : (
+                    <NavLink to="/SignIn" className="header__nav__link">
+                        <i className="fa fa-user-circle"></i>
+                        <p>Sign In</p>
+                    </NavLink>
+                )}
             </nav>
         </header>
     )
